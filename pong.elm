c550f92fc6dbc6c0037b38ae1d6d2920c5ea784f
Daniel Miller <danm1113@gmail.com>
2015-07-13 00:25:35 -0700

    Beginning work. State has a dummy interface for testing input.

--- pong.elm
+++ pong.elm
@@ -1,12 +1,39 @@
-import Time exposing (Time)
-import Text exposing (fromString)
+import Time exposing (Time, inSeconds, fps, second)
+import Text exposing (fromString, Text)
 import Graphics.Element exposing (..)
-
+import Keyboard
+import Signal exposing (..)
+{-
+  inputs for the game. Space acts as a pause.
+-}
 type alias Input =
   { space: Bool
   , paddle1: Int
   , paddle2: Int
   , delta: Time }
 
-main = fromString "Hello World"
-  |> leftAligned
+delta: Signal Time
+delta =
+  Signal.map inSeconds (fps 35)
+
+input: Signal Input
+input =
+  Signal.map4 Input
+    Keyboard.space
+    (Signal.map .y Keyboard.wasd)
+    (Signal.map .y Keyboard.arrows)
+    delta
+  |> Signal.sampleOn delta
+
+defaultMessage: String
+defaultMessage = "Hello World"
+
+inputToMessage: Input -> Text
+inputToMessage input =
+  if | input.paddle1 ==  1 -> fromString "Up"
+     | input.paddle1 == -1 -> fromString "Down"
+     | otherwise           -> fromString defaultMessage
+
+main =
+  inputToMessage <~ input
+    |> Signal.map leftAligned
